7c268826a1a7d73e223e0cc3f62b6d258bb64a78
phantomy2kcia <phantomy2kcia@gmail.com>
2018-05-01 20:39:13 +0900

    parcel

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log('hello world lalal');
+import React from "react";
+import ReactDom from "react-dom";
+import App from "./src/App";
+import typography from "./typography";
+
+ReactDom.render(<App />, document.getElementById('app'));
---
+++ src/App.js
@@ -0,0 +1,4 @@
+import React from "react";
+import styles from "./style.scss"
+
+export default () => <div className = {styles.title}><h1>Hello</h1><p>loreum ipsum</p></div>
